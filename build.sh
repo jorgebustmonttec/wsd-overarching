8dd74db8086b4a1ac16e2d06341d6e9cdea35a23
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-20 02:47:34 +0300

    working tailwind

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 rm -rf node_modules .deno .vite .cache package-lock.json deno.lock
+cd client
+rm -rf node_modules .deno .vite .cache package-lock.json deno.lock
+cd ..
+cd server
+rm -rf node_modules .deno .vite .cache package-lock.json deno.lock
+cd ..
 docker compose down
 docker compose up --build
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,24 +1,17 @@
-<form class="max-w-sm mx-auto mt-6">
-  <h2 class="text-2xl mb-4">Create a todo</h2>
-  <div class="mb-4">
-    <label for="name">Name</label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      class="w-full"
-      placeholder="Enter name"
-    />
-  </div>
+<form class="mx-auto w-full max-w-md space-y-4">
+  <h2 class="h2">Create a todo</h2>
 
-  <div class="mb-4 items-center">
-    <input type="checkbox" id="completed" name="completed" />
-    <label for="completed" class="ml-2">Completed</label>
-  </div>
+  <label for="name" class="label">
+    <span class="label-text">Name</span>
+    <input class="input" type="text" id="name" name="name" />
+  </label>
 
-  <input
-    type="submit"
-    class="w-full bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700"
-    value="Create"
-  />
+  <label class="flex items-center space-x-2">
+    <input class="checkbox" type="checkbox" id="completed" name="completed" />
+    <p>Completed</p>
+  </label>
+
+  <button type="submit" class="w-full btn preset-filled-primary-500">
+    Create
+  </button>
 </form>
