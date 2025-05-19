821f94b55e7b24787065376755126f36e1d5a47c
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-20 02:34:30 +0300

    tailwind forms

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+rm -rf node_modules .deno .vite .cache package-lock.json deno.lock
+docker compose down
 docker compose up --build
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,24 @@
-<script>
-  let count = $state(0);
-</script>
+<form class="max-w-sm mx-auto mt-6">
+  <h2 class="text-2xl mb-4">Create a todo</h2>
+  <div class="mb-4">
+    <label for="name">Name</label>
+    <input
+      type="text"
+      id="name"
+      name="name"
+      class="w-full"
+      placeholder="Enter name"
+    />
+  </div>
 
-<h1>Hello world!</h1>
-<p>Count: {count}</p>
-<button onclick={() => count++}>increment</button>
+  <div class="mb-4 items-center">
+    <input type="checkbox" id="completed" name="completed" />
+    <label for="completed" class="ml-2">Completed</label>
+  </div>
+
+  <input
+    type="submit"
+    class="w-full bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700"
+    value="Create"
+  />
+</form>
