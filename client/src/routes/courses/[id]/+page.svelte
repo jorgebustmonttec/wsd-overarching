443300fc798fa778a68d44f3d09c25db3165f43c
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-20 19:10:40 +0300

    minor edits

--- client/src/routes/courses/[id]/+page.svelte
+++ client/src/routes/courses/[id]/+page.svelte
@@ -1,17 +1,9 @@
 <script>
-  import { onMount } from "svelte";
   import Questions from "$lib/components/question/Questions.svelte";
-  import { courseApi } from "$lib/services/courseApi.js";
 
   let { data } = $props();
-  let courseName = "";
-
-  onMount(async () => {
-    const course = await courseApi.getCourse(data.id);
-    courseName = course.name;
-  });
 </script>
 
-<h1>Course {courseName}</h1>
+<h1>Course {data.id}</h1>
 
 <Questions courseId={data.id} />
