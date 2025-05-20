5de7f175f0adb469e76564563445267a4b2d9e21
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-20 04:18:38 +0300

    update build.sh and clean files

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
+clear
+clear
+echo "Building the project..."
+echo "Cleaning up old build files..."
+rm -rf node_modules .deno .vite .cache package-lock.json deno.lock
+cd client
+rm -rf node_modules .deno .vite .cache package-lock.json deno.lock
+cd ..
+cd server
+rm -rf node_modules .deno .vite .cache package-lock.json deno.lock
+cd ..
+echo "Building the client..."
+docker compose down
 docker compose up --build
