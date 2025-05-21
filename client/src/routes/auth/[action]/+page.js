7f48bdaa4ad4d58255be3c38cf1493cf7e7ea596
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-22 01:14:54 +0300

    BASE SKELETON FFS

--- client/src/routes/auth/[action]/+page.js
+++
@@ -1,9 +0,0 @@
-import { error } from "@sveltejs/kit";
-
-export const load = ({ params }) => {
-  if (params.action !== "login" && params.action !== "register") {
-    throw error(404, "Page not found.");
-  }
-
-  return params;
-};
--- server/app.js
+++ server/app.js
@@ -13,48 +13,4 @@ app.use("/*", logger());
 app.get("/", (c) => c.json({ message: "Hello world!" }));
 
 
-
-app.post("/api/auth/register", async (c) => {
-  const data = await c.req.json();
-  const email = data.email.trim().toLowerCase();
-  const password = data.password.trim();
-
-  try {
-    await sql`INSERT INTO users (email, password_hash)
-      VALUES (${email}, ${hash(password)})`;
-  } catch (_e) {
-    // Do nothing on error (e.g. duplicate email), still respond below
-  }
-
-  return c.json({
-    message: `Confirmation email sent to address ${email}.`,
-  });
-});
-
-app.post("/api/auth/login", async (c) => {
-  const data = await c.req.json();
-  const email = data.email.trim().toLowerCase();
-  const password = data.password.trim();
-
-  const result = await sql`SELECT * FROM users WHERE email = ${email}`;
-
-  if (result.length === 0) {
-    c.status(401);
-    return c.json({ message: "Invalid email or password!" });
-  }
-
-  const user = result[0];
-  const passwordValid = verify(password, user.password_hash);
-
-  if (!passwordValid) {
-    c.status(401);
-    return c.json({ message: "Invalid email or password!" });
-  }
-
-  return c.json({
-    message: `Logged in as user with id ${user.id}`,
-  });
-});
-
-
 export default app;
