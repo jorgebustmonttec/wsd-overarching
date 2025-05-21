059ab2627a560d30cd862a9a46e8e20e0ccec1af
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-22 00:54:52 +0300

    login and register server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
 import postgres from "postgres";
+import { hash, verify } from "jsr:@denorg/scrypt@4.4.4";
 
 const app = new Hono();
 const sql = postgres();        // creds come from project.env
@@ -11,9 +12,49 @@ app.use("/*", logger());
 
 app.get("/", (c) => c.json({ message: "Hello world!" }));
 
-app.get("/todos", async (c) => {
-  const todos = await sql`SELECT * FROM todos`;
-  return c.json(todos);
+
+
+app.post("/api/auth/register", async (c) => {
+  const data = await c.req.json();
+  const email = data.email.trim().toLowerCase();
+  const password = data.password.trim();
+
+  try {
+    await sql`INSERT INTO users (email, password_hash)
+      VALUES (${email}, ${hash(password)})`;
+  } catch (_e) {
+    // Do nothing on error (e.g. duplicate email), still respond below
+  }
+
+  return c.json({
+    message: `Confirmation email sent to address ${email}.`,
+  });
 });
+
+app.post("/api/auth/login", async (c) => {
+  const data = await c.req.json();
+  const email = data.email.trim().toLowerCase();
+  const password = data.password.trim();
+
+  const result = await sql`SELECT * FROM users WHERE email = ${email}`;
+
+  if (result.length === 0) {
+    c.status(401);
+    return c.json({ message: "Invalid email or password!" });
+  }
+
+  const user = result[0];
+  const passwordValid = verify(password, user.password_hash);
+
+  if (!passwordValid) {
+    c.status(401);
+    return c.json({ message: "Invalid email or password!" });
+  }
+
+  return c.json({
+    message: `Logged in as user with id ${user.id}`,
+  });
+});
+
 
 export default app;
