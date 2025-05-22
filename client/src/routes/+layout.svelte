a811a3a0f461336589b81681f70be967df95ffbb
Jorge Bustamante <jemilianobustamante@gmail.com>
2025-05-23 01:57:25 +0300

    idc

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,10 +1,17 @@
 <script>
+  import { useUserState } from "$lib/states/userState.svelte.js";
   import "../app.css";
+
   let { children, data } = $props();
+
+  const userState = useUserState();
+  if (data.user) {
+    userState.user = data.user;
+  }
 </script>
 
-{#if data.user}
-  <p>Hello {data.user}!</p>
+{#if data.user?.id}
+  <p>Hello {data.user?.id}!</p>
 {/if}
 
 <main class="container mx-auto max-w-lg">
--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+
+// ============================ IMPORTS ============================
 import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
@@ -6,9 +8,26 @@ import { hash, verify } from "jsr:@denorg/scrypt@4.4.4";
 import { getCookie, setCookie } from "jsr:@hono/hono@4.6.5/cookie";
 import * as jwt from "jsr:@hono/hono@4.6.5/jwt";
 
+
+// ============================ APP SETUP ============================
+
+// ============== CONSTANTS ==============
 const app = new Hono();
 const sql = postgres();        // creds come from project.env
 
+
+const COOKIE_KEY = "auth";
+const JWT_SECRET = "secret";
+
+const userMiddleware = async (c, next) => {
+  const token = getCookie(c, COOKIE_KEY);
+  const { payload } = jwt.decode(token, JWT_SECRET);
+  c.user = payload;
+  await next();
+};
+
+
+// ============== MIDDLEWARE ==============
 app.use(
   "/*",
   cors({
@@ -16,12 +35,31 @@ app.use(
     credentials: true,
   }),
 );
+
 app.use("/*", logger());
 
+app.use(
+  "/api/notes/*",
+  jwt.jwt({
+    cookie: COOKIE_KEY,
+    secret: JWT_SECRET,
+  }),
+);
+
+
+// then, extract the user identifier from the token
+app.use("/api/notes/*", userMiddleware);
+
+
+
+
+// ============================ ENDPOINTS ============================
+
+
+// ============== BASE ==============
 app.get("/", (c) => c.json({ message: "Hello world!" }));
 
-const COOKIE_KEY = "auth";
-
+// ============== REGISTER ==============
 app.post("/api/auth/register", async (c) => {
   const data = await c.req.json();
   const email = data.email.trim().toLowerCase();
@@ -39,8 +77,7 @@ app.post("/api/auth/register", async (c) => {
   });
 });
 
-
-
+// ============== LOGIN ==============
 app.post("/api/auth/login", async (c) => {
   const data = await c.req.json();
 
@@ -57,8 +94,16 @@ app.post("/api/auth/login", async (c) => {
 
   const passwordValid = verify(data.password.trim(), user.password_hash);
   if (passwordValid) {
-    // setting the cookie as the user id
-    setCookie(c, COOKIE_KEY, user.id, {
+    // define the payload
+    const payload = {
+      id: user.id,
+    };
+
+    // create the token by signing the payload
+    const token = await jwt.sign(payload, JWT_SECRET);
+
+    // set the token as the cookie value
+    setCookie(c, COOKIE_KEY, token, {
       path: "/",
       domain: "localhost",
       httpOnly: true,
@@ -72,8 +117,66 @@ app.post("/api/auth/login", async (c) => {
       "message": "Invalid password!",
     });
   }
+  
 });
 
+// ============== VERIFY ==============
+// ...
+app.post("/api/auth/verify", async (c) => {
+  const token = getCookie(c, COOKIE_KEY);
+  if (!token) {
+    c.status(401);
+    return c.json({
+      "message": "No token found!",
+    });
+  }
+
+  try {
+    const payload = await jwt.verify(token, JWT_SECRET);
+    payload.exp = Math.floor(Date.now() / 1000) + 60;
+
+    const refreshedToken = await jwt.sign(payload, JWT_SECRET);
+
+    setCookie(c, COOKIE_KEY, refreshedToken, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    return c.json({
+      "message": "Valid token!",
+    });
+  } catch (e) {
+    c.status(401);
+    return c.json({
+      "message": "Invalid token!",
+    });
+  }
+});
+
+// ============= notes ==============
+
+app.get("/api/notes", async (c) => {
+  return c.json(["C", "D", "E", "F", "G", "A", "B"]);
+});
+
+app.post("/api/notes", async (c) => {
+  const { text } = await c.req.json();
+  const result = await sql`INSERT INTO notes (user_id, text)
+    VALUES (${c.user.id}, ${text}) RETURNING *`;
+  return c.json(result[0]);
+});
+
+app.get("/api/notes/:id", async (c) => {
+  const notes = await sql`SELECT * FROM notes
+    WHERE id = ${c.req.param("id")} AND user_id = ${c.user.id}`;
+  if (notes.length <= 0) {
+    c.status(404);
+    return c.json({ error: "Note not found" });
+  }
+  return c.json(notes[0]);
+});
 
 
 export default app;
